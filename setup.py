8778b96d71addc67c2fba16771803cebdf7f53d1
pablo pernías <pablo@pernias.com>
2024-01-03 11:27:16 +0100

    add a webdataset path filler

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="warp_core",
     packages=find_packages(),
     description="Modular Extensible Training Framework",
-    version="0.0.1",
+    version="0.0.2",
     url="https://github.com/WARP-AI/WarpCore",
     author="Pablo Pernías",
     author_email="pablo@pernias.com",
--- warp_core/data/__init__.py
+++ warp_core/data/__init__.py
@@ -1,4 +1,7 @@
 import json
+import subprocess
+import yaml
+import os
 
 class MultiFilter():
     def __init__(self, rules, default=False):
@@ -38,3 +41,24 @@ class MultiGetter():
         if len(outputs) == 1:
             outputs = outputs[0]
         return outputs
+
+def setup_webdataset_path(paths, cache_path=None):
+    if cache_path is None or not os.path.exists(cache_path):
+        tar_paths = []
+        if isinstance(paths, str):
+            paths = [paths]
+        for path in paths:
+            bucket = "/".join(path.split("/")[:3])
+            result = subprocess.run([f"aws s3 ls {path} --recursive | awk '{{print $4}}'"], stdout=subprocess.PIPE, shell=True, check=True)
+            files = result.stdout.decode('utf-8').split()
+            files = [f"{bucket}/{f}" for f in files if f.endswith(".tar")]
+            tar_paths += files
+
+        with open(cache_path, 'w', encoding='utf-8') as outfile:
+            yaml.dump(tar_paths, outfile, default_flow_style=False)
+    else:
+        with open(cache_path, 'r', encoding='utf-8') as file:
+            tar_paths = yaml.safe_load(file)
+
+    tar_paths_str = ",".join([f"{p}" for p in tar_paths])
+    return f"pipe:aws s3 cp {{ {tar_paths_str} }} -"
